1f9f9de8994e2e34d9001f7c8735fd00689325d9
Aparna-devi-chinnu <aparnarajavel2@gmail.com>
2025-08-31 01:46:26 +0530

    Clean up

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Counter extends Component{
     state = {
-        count:0,
         tags: ["tag1","tag2","tag3","tag4"]
-    }
-
-    style = {
-        fontSize:15,
-        fontColor:"black"
     }
     render(){
 
@@ -18,26 +12,11 @@ class Counter extends Component{
         return(
             // if we dont want a extra div use React.Fragment
             <React.Fragment>  
-                <span style={this.style} className={classes}>
-                    {this.getCount()}
-                </span>
-                <button className="btn btn-secondary btn-sm">Click me </button>
                 <ul>
                     {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
                 </ul>
             </React.Fragment>
         )
-    }
-
-    getClasses() {
-        let classes = "badge m-2 bg-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-
-    getCount(){
-    const {count} = this.state // only the count variable is got here 
-    return count === 0 ? "Zero" : count;
     }
 }
 export default Counter;
