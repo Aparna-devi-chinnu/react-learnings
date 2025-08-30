5f70a8d27ec8d33b66ff28c1e26edf592cc76ab3
Aparna-devi-chinnu <aparnarajavel2@gmail.com>
2025-08-31 01:51:27 +0530

    Added logic to render content based on conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,16 +5,20 @@ class Counter extends Component{
     state = {
         tags: ["tag1","tag2","tag3","tag4"]
     }
+
+    renderTags(){
+        if(this.state.tags.length == 0){
+            return "There are no tags"
+        }else{
+            return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+        }
+    }
     render(){
-
-        let classes = this.getClasses(); 
-
         return(
             // if we dont want a extra div use React.Fragment
             <React.Fragment>  
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                {this.state.tags.length == 0 && "Please create new tag!!!"}
+                {this.renderTags()}
             </React.Fragment>
         )
     }
